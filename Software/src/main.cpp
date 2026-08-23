629fe0f40507f4983a283d944add63f90f547e72
morena1001 <josueflores15.jf@gmail.com>
2026-08-23 12:11:56 -0500

    Commented out code for the rest of the project. I should however make branches instead of this. Was able to write pixels to the screen, but the values of each color is reversed. Also need to figure out where the smaller screen lies on the RAM.

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -1,53 +1,225 @@
 #include "Arduino.h"
-#include "Preferences.h"
+#include "SPI.h"
 
-#include "camera.h"
+#define ST7789V3_CS_PIN    43 
+#define ST7789V3_DC_PIN    6 
+#define ST7789V3_RST_PIN   5 
 
-#define CAPTURE_PIN     1
-#define SD_CARD_PIN     44
-#define DB_DELAY        50
-
-Camera camera;
-unsigned long capture_last_db_time = 0;
-int capture_state = LOW;
-int capture_last_state = LOW;
-
-Preferences preferences;
+#define ST7789V3_CMD_SWRESET    0x01
+#define ST7789V3_CMD_SLPIN      0x10
+#define ST7789V3_CMD_SLPOUT     0x11
+#define ST7789V3_CMD_DISPOFF    0x28
+#define ST7789V3_CMD_DISPON     0x29
+#define ST7789V3_CMD_CASET      0x2A
+#define ST7789V3_CMD_RASET      0x2B
+#define ST7789V3_CMD_RAMWR      0x2C
+#define ST7789V3_CMD_MADCTL     0x36
+#define ST7789V3_CMD_IDMOFF     0x38
+#define ST7789V3_CMD_COLMOD     0x3A
+#define ST7789V3_CMD_WRMEMC     0x3C
+#define ST7789V3_CMD_WRDISBV    0x51
+#define ST7789V3_CMD_WRCTRLD    0x53
 
 void setup () {
     Serial.begin (115200);
     while (!Serial);
 
-    if (camera.Init_Camera () != ESP_OK)    return;
+    // CS pin is low active
+    pinMode (ST7789V3_CS_PIN, OUTPUT);
+    digitalWrite (ST7789V3_CS_PIN, HIGH);
+    
+    // DC pin: low for command, high for data
+    pinMode (ST7789V3_DC_PIN, OUTPUT);
+    digitalWrite (ST7789V3_DC_PIN, LOW);
 
-    if (!SD.begin (SD_CARD_PIN))            return;
-    if (SD.cardType () == CARD_NONE)        return;
+    // RST pin is low active
+    pinMode (ST7789V3_RST_PIN, OUTPUT);
+    digitalWrite (ST7789V3_RST_PIN, LOW);
+    digitalWrite (ST7789V3_RST_PIN, HIGH);
+    
+    // 5 ms needed after hardware reset to send commands (p.41)
+    delay (200); 
 
-    pinMode (CAPTURE_PIN, INPUT_PULLUP);
+    SPI.begin ();
 
-    preferences.begin ("memory", false);
-    camera.Set_Image_Count (preferences.getUInt ("counter", 1));
+    // Software reset
+    digitalWrite (ST7789V3_CS_PIN, LOW);
+    digitalWrite (ST7789V3_DC_PIN, LOW);
+    SPI.transfer (ST7789V3_CMD_SWRESET);
+    digitalWrite (ST7789V3_CS_PIN, HIGH);
 
-    Serial.println ("Begin photo capture");
+    // 5 ms needed after software reset to send commands (p.122)
+    delay (200); 
+
+    // Sleep out
+    digitalWrite (ST7789V3_CS_PIN, LOW);
+    digitalWrite (ST7789V3_DC_PIN, LOW);
+    SPI.transfer (ST7789V3_CMD_SLPOUT);
+    digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+    // 120 ms needed after hardware reset to send commands (p.143)
+    delay (200); 
+
+    // Write display brightness (0x7F is half brightness)
+    digitalWrite (ST7789V3_CS_PIN, LOW);
+    digitalWrite (ST7789V3_DC_PIN, LOW);
+    SPI.transfer (ST7789V3_CMD_WRDISBV);
+    digitalWrite (ST7789V3_DC_PIN, HIGH);
+    SPI.transfer (0x7F);
+    digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+    // Set color mode (262k colo RGB interface, 18 bits / pixel)
+    digitalWrite (ST7789V3_CS_PIN, LOW);
+    digitalWrite (ST7789V3_DC_PIN, LOW);
+    SPI.transfer (ST7789V3_CMD_COLMOD);
+    digitalWrite (ST7789V3_DC_PIN, HIGH);
+    SPI.transfer (0x06);
+    digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+    // Memory data access control
+    digitalWrite (ST7789V3_CS_PIN, LOW);
+    digitalWrite (ST7789V3_DC_PIN, LOW);
+    SPI.transfer (ST7789V3_CMD_MADCTL);
+    digitalWrite (ST7789V3_DC_PIN, HIGH);
+    SPI.transfer (0x00);
+    digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+    // Write CTRL display
+    digitalWrite (ST7789V3_CS_PIN, LOW);
+    digitalWrite (ST7789V3_DC_PIN, LOW);
+    SPI.transfer (ST7789V3_CMD_WRCTRLD);
+    digitalWrite (ST7789V3_DC_PIN, HIGH);
+    SPI.transfer (0x00);
+    digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+    // // Idle mode off
+    // digitalWrite (ST7789V3_CS_PIN, LOW);
+    // digitalWrite (ST7789V3_DC_PIN, LOW);
+    // SPI.transfer (ST7789V3_CMD_IDMOFF);
+    // digitalWrite (ST7789V3_CS_PIN, HIGH);
+    
+
+    // Display on
+    digitalWrite (ST7789V3_CS_PIN, LOW);
+    digitalWrite (ST7789V3_DC_PIN, LOW);
+    SPI.transfer (ST7789V3_CMD_DISPON);
+    digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+    // Set entire screen to white
+
+        // Set column window
+        digitalWrite (ST7789V3_CS_PIN, LOW);
+        digitalWrite (ST7789V3_DC_PIN, LOW);
+        SPI.transfer (ST7789V3_CMD_CASET);
+        digitalWrite (ST7789V3_DC_PIN, HIGH);
+        byte col_win[4] = { 0x00, 0x00, 0x00, 0xEE };
+        SPI.transfer (col_win, 4);
+        digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+        // Set row window
+        digitalWrite (ST7789V3_CS_PIN, LOW);
+        digitalWrite (ST7789V3_DC_PIN, LOW);
+        SPI.transfer (ST7789V3_CMD_RASET);
+        digitalWrite (ST7789V3_DC_PIN, HIGH);
+        byte row_win[4] = { 0x00, 0x00, 0x01, 0x3E };
+        SPI.transfer (row_win, 4);
+        digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+        // Write pixel data
+        digitalWrite (ST7789V3_CS_PIN, LOW);
+        digitalWrite (ST7789V3_DC_PIN, LOW);
+        SPI.transfer (ST7789V3_CMD_RAMWR);
+        digitalWrite (ST7789V3_DC_PIN, HIGH);
+        byte pixel_data[3] = { 0x00, 0x00, 0x00 };
+        for (int i = 0; i < 239; i++) {
+            for (int j = 0; j < 319; j++) {
+                // SPI.transfer (pixel_data, 3);
+                SPI.transfer (pixel_data[0]);
+                SPI.transfer (pixel_data[1]);
+                SPI.transfer (pixel_data[2]);
+            }
+        }
+        digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+
+    // Write a box to the screen
+
+        // Set column window
+        digitalWrite (ST7789V3_CS_PIN, LOW);
+        digitalWrite (ST7789V3_DC_PIN, LOW);
+        SPI.transfer (ST7789V3_CMD_CASET);
+        digitalWrite (ST7789V3_DC_PIN, HIGH);
+        byte col_win_1[4] = { 0x00, 0x62, 0x00, 0x7C };
+        // byte col_win[4] = { 0x00, 0x84, 0x00, 0xAE };
+        SPI.transfer (col_win_1, 4);
+        digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+        // Set row window
+        digitalWrite (ST7789V3_CS_PIN, LOW);
+        digitalWrite (ST7789V3_DC_PIN, LOW);
+        SPI.transfer (ST7789V3_CMD_RASET);
+        digitalWrite (ST7789V3_DC_PIN, HIGH);
+        byte row_win_1[4] = { 0x00, 0x85, 0x00, 0xA9 };
+        // byte row_win[4] = { 0x00, 0xC7, 0x00, 0xDB };
+        SPI.transfer (row_win_1, 4);
+        digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+        // Write pixel data
+        byte pixel_data_1[3] = { 0xFC, 0xFC, 0x7E };
+        digitalWrite (ST7789V3_CS_PIN, LOW);
+        digitalWrite (ST7789V3_DC_PIN, LOW);
+        SPI.transfer (ST7789V3_CMD_RAMWR);
+        digitalWrite (ST7789V3_DC_PIN, HIGH);
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 36; j++) {
+                // SPI.transfer (pixel_data, 3);
+                SPI.transfer (pixel_data_1[0]);
+                SPI.transfer (pixel_data_1[1]);
+                SPI.transfer (pixel_data_1[2]);
+            }
+        }
+        digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+
+    // Write a box to the screen
+
+        // Set column window
+        digitalWrite (ST7789V3_CS_PIN, LOW);
+        digitalWrite (ST7789V3_DC_PIN, LOW);
+        SPI.transfer (ST7789V3_CMD_CASET);
+        digitalWrite (ST7789V3_DC_PIN, HIGH);
+        // byte col_win[4] = { 0x00, 0x62, 0x00, 0x7C };
+        byte col_win_2[4] = { 0x00, 0x84, 0x00, 0xAE };
+        SPI.transfer (col_win_2, 4);
+        digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+        // Set row window
+        digitalWrite (ST7789V3_CS_PIN, LOW);
+        digitalWrite (ST7789V3_DC_PIN, LOW);
+        SPI.transfer (ST7789V3_CMD_RASET);
+        digitalWrite (ST7789V3_DC_PIN, HIGH);
+        // byte row_win[4] = { 0x00, 0x85, 0x00, 0xA9 };
+        byte row_win_2[4] = { 0x00, 0xC7, 0x00, 0xDB };
+        SPI.transfer (row_win_2, 4);
+        digitalWrite (ST7789V3_CS_PIN, HIGH);
+
+        // Write pixel data
+        byte pixel_data_2[3] = { 0xFC, 0xFC, 0x00 };
+        digitalWrite (ST7789V3_CS_PIN, LOW);
+        digitalWrite (ST7789V3_DC_PIN, LOW);
+        SPI.transfer (ST7789V3_CMD_RAMWR);
+        digitalWrite (ST7789V3_DC_PIN, HIGH);
+        for (int i = 0; i < 42; i++) {
+            for (int j = 0; j < 20; j++) {
+                // SPI.transfer (pixel_data, 3);
+                SPI.transfer (pixel_data_2[0]);
+                SPI.transfer (pixel_data_2[1]);
+                SPI.transfer (pixel_data_2[2]);
+            }
+        }
+        digitalWrite (ST7789V3_CS_PIN, HIGH);
 }
 
 void loop () {
-    int capture_reading = digitalRead (CAPTURE_PIN);
 
-    if (capture_reading != capture_last_state)  capture_last_db_time = millis ();
-
-    if ((millis () - capture_last_db_time) > DB_DELAY && capture_reading != capture_state) {
-        capture_state = capture_reading;
-        if (capture_state == LOW) {
-            char file_name [32];
-            sprintf (file_name, "/image%d.jpg", camera.Get_Image_Count ());
-
-            camera.Photo_Save ();
-            
-            Serial.printf ("Saved picture: %s\n", file_name);
-            preferences.putUInt ("counter", camera.Get_Image_Count ());
-        }
-    }
-
-    capture_last_state = capture_reading;
 }
